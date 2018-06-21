834d01811cbcaed1cf5d4b7a1baccf937b29408e
Mark Suzuki <markdsuzuki@gmail.com>
2018-06-20 23:02:03 -0700

    working clock

--- src/App.js
+++ src/App.js
@@ -3,18 +3,52 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
+  constructor(props){
+    super(props)
+    this.state = this.getTime()
+  }
+
+  componentDidMount() {
+    this.setTimer();
+  }
+
+  componentWillUnmount(){
+    if(this.timeout) {
+      clearTimeout(this.timeout)
+    }
+  }
+
+  setTimer() {
+    this.timeout = setTimeout(this.updateClock.bind(this), 1000);
+  }
+
+  updateClock() {
+    this.setState(this.getTime, this.setTimer);
+  }
+
+  getTime() {
+    const currentTime = new Date();
+    return {
+      hours: currentTime.getHours(),
+      minutes: currentTime.getMinutes(),
+      seconds: currentTime.getSeconds(),
+      ampm: currentTime.getHours() >= 12 ? 'pm' : 'am'
+    }
+  }
+  render(){
+    const {hours, minutes, seconds, ampm} = this.state
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="clock ">
+        {hours === 0 ? 12 :
+          (hours > 12) ?
+          hours - 12 : hours
+        }:{
+          minutes > 9 ? minutes: `0${minutes}`
+        }:{
+          seconds > 9 ? seconds: `0${seconds}`
+        } {ampm}
       </div>
-    );
+    )
   }
 }
 
